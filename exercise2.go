6fd53cce45ad103c0b0c07df88f019a42bed44ce
Haasin Farooq <haasin.farooq@tintash.com>
2021-06-15 02:15:24 +0500

    complete write files exercise

---
+++ exercise2.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func createFile(filename string) {
+	f, err := os.Create(filename)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%v file created successfully\n", filename)
+
+	defer f.Close()
+}
+
+func writeFile(filename, content string) {
+	f, err := os.OpenFile(filename, os.O_WRONLY, 0644)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = f.WriteString(content)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%v file written successfully\n", filename)
+
+	defer f.Close()
+}
+
+func readFile(filename string) {
+	f, err := os.OpenFile(filename, os.O_RDONLY, 0644)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	content, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%v file read successfully\n", filename)
+	fmt.Printf("Content: %v\n", string(content))
+
+	defer f.Close()
+}
+
+func main() {
+	createFile("sample.txt")
+	writeFile("sample.txt", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+	readFile("sample.txt")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/exercise2
+
+go 1.16
